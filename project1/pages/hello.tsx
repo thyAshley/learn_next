c61e0b60cc6ccb27625fdc1c3a9ef2ff7a448456
thyashley <thy.ashley@gmail.com>
2021-10-28 21:49:49 +0800

    Getting styled component to work with nextjs

--- project1/pages/hello.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from "next";
-import React from "react";
-import { StyledContainer } from "../styles/hello/styles";
-
-const hello: NextPage = () => {
-  return <StyledContainer>hey</StyledContainer>;
-};
-
-export default hello;
