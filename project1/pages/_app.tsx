dd97f694566d32f112d4d311bd56d43e2e178004
Ashley Teo <ashley.teo@ufinity.com>
2021-11-08 15:24:11 +0800

    add context and usereducer

--- project1/pages/_app.tsx
+++ project1/pages/_app.tsx
@@ -1,12 +1,75 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import React, { createContext, ReducerAction, useReducer } from 'react';
+
+export const storeContext = createContext(
+	{} as { state: State; dispatch: React.Dispatch<any> }
+);
+
+interface StoreProviderProps {
+	children: React.ReactNode;
+}
+
+export enum ActionKind {
+	SET_LAT_LONG = 'SET_LAT_LONG',
+	SET_STORES = 'SET_STORES',
+}
+
+type Action =
+	| {
+			type: ActionKind;
+			payload: any;
+	  }
+	| {
+			type: 'SET_LAT_LONG';
+			payload: {
+				latLong: string;
+			};
+	  };
+
+interface State {
+	latLong: string;
+	stores: any[];
+}
+
+const storeReducer = (state: State, action: Action) => {
+	switch (action.type) {
+		case ActionKind.SET_LAT_LONG: {
+			return {
+				...state,
+				latLong: action.payload.latLong,
+			};
+		}
+		case ActionKind.SET_STORES: {
+			return state;
+		}
+		default:
+			throw new Error('Unhandled action type');
+	}
+};
+
+const initialValues = {
+	latLong: '',
+	stores: [],
+};
+
+const StoreProvider = ({ children }: StoreProviderProps) => {
+	const [state, dispatch] = useReducer(storeReducer, initialValues);
+	return (
+		<storeContext.Provider value={{ state, dispatch }}>
+			{children}
+		</storeContext.Provider>
+	);
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Component {...pageProps} />
-    </>
-  );
+	return (
+		<>
+			<StoreProvider>
+				<Component {...pageProps} />
+			</StoreProvider>
+		</>
+	);
 }
 
 export default MyApp;
--- project1/pages/coffee-store/[id].tsx
+++ project1/pages/coffee-store/[id].tsx
@@ -1,59 +1,59 @@
-import { GetStaticPaths, GetStaticProps, NextPage } from "next";
-import Link from "next/link";
-import { useRouter } from "next/dist/client/router";
-import React from "react";
+import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
+import Link from 'next/link';
+import { useRouter } from 'next/dist/client/router';
+import React, { useEffect } from 'react';
 
-import CoffeeShop from "../../data/CoffeeShop.json";
-import { coffeeStoreDetails } from "../../types";
-import Head from "next/head";
+import CoffeeShop from '../../data/CoffeeShop.json';
+import { coffeeStoreDetails } from '../../types';
+import Head from 'next/head';
 
 interface CoffeeShopDetailPageProps {
-  coffeeStores: coffeeStoreDetails;
+	coffeeStores: coffeeStoreDetails;
 }
 
 const CoffeeShopDetailPage: NextPage<CoffeeShopDetailPageProps> = ({
-  coffeeStores,
+	coffeeStores,
 }) => {
-  const router = useRouter();
-  const { id } = router.query;
-  if (router.isFallback) {
-    return <div>Loading...</div>;
-  }
-  return (
-    <>
-      <Head>
-        <title>{coffeeStores.name}</title>
-      </Head>
-      <div>
-        <Link href="/" prefetch>
-          <a>Back to home</a>
-        </Link>
-        <p>{coffeeStores.address}</p>
-        <p>{coffeeStores.name}</p>
-        <p>{coffeeStores.neighbourhood}</p>
-      </div>
-    </>
-  );
+	const router = useRouter();
+	const { id } = router.query;
+	if (router.isFallback) {
+		return <div>Loading...</div>;
+	}
+	return (
+		<>
+			<Head>
+				<title>{coffeeStores.name}</title>
+			</Head>
+			<div>
+				<Link href="/" prefetch>
+					<a>Back to home</a>
+				</Link>
+				<p>{coffeeStores.address}</p>
+				<p>{coffeeStores.name}</p>
+				<p>{coffeeStores.neighbourhood}</p>
+			</div>
+		</>
+	);
 };
 
 export const getStaticProps: GetStaticProps = ({ params }) => {
-  return {
-    props: {
-      coffeeStores: CoffeeShop.find(
-        (coffeeStore) => coffeeStore.id === Number(params.id)
-      ),
-    },
-  };
+	return {
+		props: {
+			coffeeStores: CoffeeShop.find(
+				(coffeeStore) => coffeeStore.id === Number(params!.id)
+			),
+		},
+	};
 };
 
 export const getStaticPaths: GetStaticPaths = () => {
-  const paths = CoffeeShop.map((shop) => ({
-    params: { id: shop.id.toString() },
-  }));
-  return {
-    paths,
-    fallback: true,
-  };
+	const paths = CoffeeShop.map((shop) => ({
+		params: { id: shop.id.toString() },
+	}));
+	return {
+		paths,
+		fallback: true,
+	};
 };
 
 export default CoffeeShopDetailPage;
